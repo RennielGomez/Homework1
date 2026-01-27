d2efc8dc1ca64937196b142b73fa4511d2c9de52
Renniel Gomez <gomezrenniel9@gmail.com>
2026-01-27 23:28:42 +0800

    homework update

---
+++ q7/main.tf
@@ -0,0 +1,58 @@
+
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.6.0"
+    }
+  }
+}
+
+ provider "google" {
+   project = var.project
+   region  = var.region
+   zone    = var.zone
+ }
+
+ /* add these data blocks */
+ 
+ # This data source gets a temporary token for the service account
+ data "google_service_account_access_token" "default" {
+   provider               = google
+   target_service_account = "<SERVICE_ACCOUNT_EMAIL>"
+   scopes                 = ["https://www.googleapis.com/auth/cloud-platform"]
+   lifetime               = "3600s"
+ }
+ 
+ # This second provider block uses that temporary token and does the real work
+ provider "google" {
+   alias        = "impersonated"
+   access_token = data.google_service_account_access_token.default.access_token
+   project      = var.project
+   region       = var.region
+   zone         = var.zone
+ }
+
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
---
+++ q7/variables.tf
@@ -0,0 +1,33 @@
+variable "project" {
+  description = "Project"
+  default     = "project-66bf4e3c-094a-4b5f-9e4"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "terraform-demo-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
